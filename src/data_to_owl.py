39f5aad14c4a97b6823178ea660ceac78e28c4ac
hkir-dev <huseyin_kir@yahoo.com>
2024-05-02 22:15:24 +0100

    yaml decorator added

--- src/data_to_owl.py
+++ src/data_to_owl.py
@@ -3,6 +3,8 @@ import os
 import logging
 from typing import List
 
+from ruamel.yaml import YAML
+
 from linkml.generators.pythongen import PythonGenerator
 from linkml_runtime import SchemaView
 
@@ -34,6 +36,8 @@ INPUT_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../schema/
 
 
 CAS_SCHEMA_IN = os.path.join(SCHEMA_DIR, 'BICAN_schema.json')
+
+BASE_LINKML_SCHEMA = os.path.join(MODEL_DIR, 'BICAN-schema.yaml')
 SCHEMA_IN = os.path.join(MODEL_DIR, 'BICAN-schema2.yaml')
 SCHEMA_IN3 = os.path.join(MODEL_DIR, 'BICAN-schema3.yaml')
 SCHEMA_EXPANDED = os.path.join(MODEL_DIR, 'BICAN-schema3-expanded.yaml')
@@ -43,6 +47,8 @@ DATA_IN3 = os.path.join(INPUT_DIR, 'AIT_MTG3.json')
 
 OWL_OUT = os.path.join(MODEL_DIR, 'AIT_MTG.ofn')
 OWL_OUT2 = os.path.join(MODEL_DIR, 'AIT_MTG_rdf.owl')
+
+CAS_NAMESPACE = "https://cellular-semantics.sanger.ac.uk/ontology/CAS"
 
 
 logger = logging.getLogger()
@@ -127,12 +133,11 @@ def run_data2owl(schema_path, data_path, output_path):
         stream.write(str(doc))
 
 
-def convert_cas_schema_to_linkml(output_path=None):
+def convert_cas_schema_to_linkml(cas_schema, output_path=None):
     ie = JsonSchemaImportEngine()
-    schema = ie.load(CAS_SCHEMA_IN, name="cell-annotation-schema", format='json', root_class_name=None)
-    model_path = os.path.join(MODEL_DIR, 'BICAN-schema.yaml')
-
-    write_schema(schema, model_path)
+    schema = ie.load(cas_schema, name="cell-annotation-schema", format='json', root_class_name=None)
+    if output_path:
+        write_schema(schema, output_path)
     return schema
 
 
@@ -159,13 +164,103 @@ def expand_schema(config: str, schema: str, value_set_names: List[str], output: 
     )
 
 
+def decorate_linkml_schema(schema_path, ontology_namespace, ontology_iri, labelsets=None, output_path=None):
+    """
+    Adds additional properties to the LinkML schema that are required for OWL conversion.
+    Parameters:
+        schema_path: str
+            Path to the LinkML schema file
+        ontology_namespace: str
+            Ontology namespace (e.g. MTG)
+        ontology_iri: str
+            Ontology IRI (e.g. https://purl.brain-bican.org/ontology/AIT_MTG/)
+        output_path: str
+            (Optional) Path to the output schema file
+    Returns:
+        Decorated schema
+    """
+    schema_obj = read_yaml(schema_path)
+
+    schema_obj["id"] = CAS_NAMESPACE
+
+    ontology_namespace = ontology_namespace.upper()
+    prefixes = {
+        "CAS": CAS_NAMESPACE + "/",
+        "General_Cell_Annotation_Open_Standard": CAS_NAMESPACE + "/",
+        "_base": ontology_iri,
+        ontology_namespace: ontology_iri,
+        "obo": "http://purl.obolibrary.org/obo/",
+        "CL": "http://purl.obolibrary.org/obo/CL_",
+        "PCL": "http://purl.obolibrary.org/obo/PCL_",
+        "RO": "http://purl.obolibrary.org/obo/RO_",
+        "skos": "http://www.w3.org/2004/02/skos/core#"
+    }
+    for labelset in labelsets:
+        prefixes[labelset] = ontology_iri + f"{labelset}#"
+
+    schema_obj["prefixes"] = prefixes
+    schema_obj["default_range"] = "string"
+    schema_obj["default_curi_maps"] = ["semweb_context", "obo_context"]
+    schema_obj["enums"]["CellTypeEnum"] = {
+              "reachable_from": {
+                  "source_ontology": "obo:cl",
+                  "source_nodes": ["CL:0000000"],
+                  "include_self": True,
+                  "relationship_types": ["rdfs:subClassOf"]
+              }
+          }
+    schema_obj["slots"]["id"] = {
+            "identifier": True,
+            "range": "uriorcurie"
+        }
+    schema_obj["slots"]["name"]["slot_uri"] = "rdfs:label"
+    schema_obj["slots"]["description"]["slot_uri"] = "IAO:0000115"
+    schema_obj["slots"]["cell_label"]["slot_uri"] = "rdfs:label"
+    schema_obj["slots"]["cell_fullname"]["slot_uri"] = "skos:preflabel"
+    schema_obj["slots"]["cell_ontology_term_id"]["slot_uri"] = "RO:0002473"
+    schema_obj["slots"]["cell_ontology_term_id"]["range"] = "CellTypeEnum"
+    schema_obj["slots"]["cell_set_accession"]["identifier"] = True
+    schema_obj["slots"]["parent_cell_set_accession"]["slot_uri"] = "RO:0015003"
+    schema_obj["slots"]["parent_cell_set_accession"]["range"] = "Annotation"
+    schema_obj["slots"]["source_taxonomy"]["range"] = "uriorcurie"
+    schema_obj["slots"]["comment"]["slot_uri"] = "IAO:0000115"
+    schema_obj["slots"]["labelset"]["slot_uri"] = "CAS:has_labelset"
+    schema_obj["slots"]["labelsets"]["inlined"] = True
+    schema_obj["slots"]["annotations"]["inlined"] = True
+    list(schema_obj["classes"]["Labelset"]["slots"]).append("id")
+    list(schema_obj["classes"]["GeneralCellAnnotationOpenStandard"]["slots"]).append("id")
+    schema_obj["classes"]["Annotation"]["class_uri"] = "PCL:0010001"
+
+    if output_path:
+        write_schema(schema_obj, output_path)
+
+
 def convert_linkml_to_linkml_owl():
     pass
 
 
+def read_yaml(file_path: str) -> dict:
+    """
+    Reads the schema file from the given path.
+    :param file_path: path to the yaml file
+    :return: yaml object
+    """
+    with open(file_path, "r") as fs:
+        try:
+            ryaml = YAML(typ="safe")
+            return ryaml.load(fs)
+        except Exception as e:
+            raise Exception("Yaml read failed:" + file_path + " " + str(e))
+
+
 if __name__ == '__main__':
-    # schema = convert_cas_schema_to_linkml()
-    expand_schema(None, SCHEMA_IN3, ["CellTypeEnum"], SCHEMA_EXPANDED)
+    # schema = convert_cas_schema_to_linkml(CAS_SCHEMA_IN, BASE_LINKML_SCHEMA)
+    decorate_linkml_schema(BASE_LINKML_SCHEMA,
+                           ontology_namespace="MTG",
+                           ontology_iri="https://purl.brain-bican.org/ontology/AIT_MTG/",
+                           labelsets=["CrossArea_cluster", "CrossArea_subclass", "Class"],
+                           output_path=os.path.join(MODEL_DIR, 'BICAN-schema-test.yaml'))
+    # expand_schema(None, SCHEMA_IN3, ["CellTypeEnum"], SCHEMA_EXPANDED)
     # run_data2owl(SCHEMA_IN, DATA_IN, OWL_OUT)
     # if os.path.exists(OWL_OUT2):
     #     os.remove(OWL_OUT2)
